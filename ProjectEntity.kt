826405579a28279cc261617ca81f8ba2a35a924b
shayanay880 <167648493+shayanay880@users.noreply.github.com>
2025-12-13 11:31:37 +0330

    Add ProjectEntity data class for Room database

---
+++ ProjectEntity.kt
@@ -0,0 +1,10 @@
+package com.example.v12
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "projects")
+data class ProjectEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String
+)
